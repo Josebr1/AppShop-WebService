d773d3b628147fe3a40ce58d71d1337317085c2f
Jose <jose.silva.br@outlook.com>
2017-05-06 22:26:18 -0300

    CRUD product

--- src/routes.php
+++ src/routes.php
@@ -110,3 +110,118 @@ $app->delete('/rest/category/delete/[{id}]', function ($request, $response, $arg
     return $this->response->withJson($status);
 });
 
+/**
+ * rest/product/*
+ *
+ *
+ * CRUD product
+ *
+ */
+// /rest/product/all -> SELECT ALL
+$app->get('/rest/product/all', function ($request, $response, $args) {
+    $sth = $this->db->prepare("SELECT * FROM produto ORDER BY nome");
+    $sth->execute();
+    $result = $sth->fetchAll();
+    return $this->response->withJson($result);
+});
+
+// /rest/product/{id} -> SELECT BY ID
+$app->get('/rest/product/[{id}]', function ($request, $response, $args) {
+    $sth = $this->db->prepare("SELECT * FROM produto WHERE id=:id");
+    $sth->bindParam("id", $args['id']);
+    $sth->execute();
+    $result = $sth->fetchAll();
+    return $this->response->withJson($result);
+});
+
+
+// /rest/product/{name} -> SELECT BY NAME
+$app->get('/rest/product/name/[{nome}]', function ($request, $response, $args) {
+    $sth = $this->db->prepare("select * from produto where lower(nome) like :nome");
+    $sth->bindParam("nome", $args['nome']);
+    $sth->execute();
+    $result = $sth->fetchAll();
+    return $this->response->withJson($result);
+});
+
+
+// /rest/product/category/{id} -> SELECT BY CATEGORY
+$app->get('/rest/product/category/[{id}]', function ($request, $response, $args) {
+    $sth = $this->db->prepare("select * from produto where id_categoria =:id");
+    $sth->bindParam("id", $args['id']);
+    $sth->execute();
+    $result = $sth->fetchAll();
+    return $this->response->withJson($result);
+});
+
+// /rest/product/add -> INSERT
+$app->post('/rest/product/add', function ($request, $response) {
+    $input = $request->getParsedBody();
+
+    $sql = "INSERT produto (nome, descricao, preco, foto, id_categoria) VALUES(:nome, :descricao, :preco, :foto, :id_categoria)";
+
+    try {
+        $sth = $this->db->prepare($sql);
+        $sth->bindParam("nome", $input['nome']);
+        $sth->bindParam("descricao", $input['descricao']);
+        $sth->bindParam("preco", $input['preco']);
+        $sth->bindParam("foto", $input['foto']);
+        $sth->bindParam("id_categoria", $input['id_categoria']);
+        $sth->execute();
+
+        $result = $sth->rowCount();
+
+        $status = "";
+
+        if ($result == 1) {
+            $status = "produto Adicionada com sucesso!";
+        }
+
+        return $this->response->withJson($status);
+    } catch (PDOException $e) {
+        return $this->response->withJson("Erro ao produto categoria");
+    }
+});
+
+// /rest/product/update/{id} -> UPDATE
+$app->put('/rest/product/update', function ($request, $response, $args) {
+    $input = $request->getParsedBody();
+
+    $sql = "UPDATE produto SET nome=:nome, descricao=:descricao, preco=:preco, foto=:foto, id_categoria=:id_categoria WHERE id=:id";
+
+    $sth = $this->db->prepare($sql);
+    $sth->bindParam("nome", $input['nome']);
+    $sth->bindParam("descricao", $input['descricao']);
+    $sth->bindParam("preco", $input['preco']);
+    $sth->bindParam("foto", $input['foto']);
+    $sth->bindParam("id_categoria", $input['id_categoria']);
+    $sth->bindParam("id", $input['id']);
+
+    $sth->execute();
+
+    $result = $sth->rowCount();
+
+    if ($result == 1) {
+        $status = "produto atualizada com sucesso!";
+    } else {
+        $status = "Erro ao atualizar produto!";
+    }
+    return $this->response->withJson($status);
+});
+
+// /rest/product/delete/{id} -> DELETE
+$app->delete('/rest/product/delete/[{id}]', function ($request, $response, $args) {
+    $sth = $this->db->prepare("DELETE FROM produto WHERE id=:id");
+    $sth->bindParam("id", $args['id']);
+    $sth->execute();
+
+    $result = $sth->rowCount();
+
+    if ($result == 1) {
+        $status = "produto deletada com sucesso!";
+    } else {
+        $status = "Erro ao deletada produto!";
+    }
+    return $this->response->withJson($status);
+});
+
